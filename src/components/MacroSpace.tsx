344abb2b4bcabd18c2f48861ffe20eec31c4d95e
Taskkill <johny95wil@gmail.com>
2019-08-23 11:33:57 +0200

    fix indentation of macrospace

--- src/components/MacroSpace.tsx
+++ src/components/MacroSpace.tsx
@@ -12,32 +12,36 @@ export default function MacroSpace (props : MacroProperties) : JSX.Element {
 
   return (
     <div className='macroSpace'>
-      <p>Built-in Macros</p>
-      <ul className='UL'>
-        { Object.entries(builtinMacros).map(([macroName, macroExpression]) =>
-          <div key={ macroName }>
-            <li className='LI'>
-              <div className='box boxMacro'>
-                { macroName } := { macroExpression }
-              </div>
-            </li>
-          </div>
-        ) }
-      </ul>
-
-      <p>User-defined Macros</p>
-      <ul>
-        { Object.entries(macroTable).map(([macroName, macroExpression]) =>
+      <div className='builtinMacros'>
+        <p>Built-in Macros</p>
+        <ul className='UL'>
+          { Object.entries(builtinMacros).map(([macroName, macroExpression]) =>
             <div key={ macroName }>
               <li className='LI'>
                 <div className='box boxMacro'>
                   { macroName } := { macroExpression }
-                  <i className="hiddenMacroIcon far fa-trash-alt" onClick={ () => removeMacro(macroName) } />
                 </div>
               </li>
             </div>
           ) }
-      </ul>
+        </ul>
+      </div>
+
+      <div className='userMacros'>      
+        <p>User-defined Macros</p>
+        <ul>
+          { Object.entries(macroTable).map(([macroName, macroExpression]) =>
+              <div key={ macroName }>
+                <li className='LI'>
+                  <div className='box boxMacro'>
+                    { macroName } := { macroExpression }
+                    <i className="hiddenMacroIcon far fa-trash-alt" onClick={ () => removeMacro(macroName) } />
+                  </div>
+                </li>
+              </div>
+            ) }
+        </ul>
+      </div>
     </div>
   )
 }
